91aa4a4c397d52fc696f4986880db70c6faea38d
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-01-06 20:23:49 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -31,149 +31,113 @@
   
   Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
-      $authedUser = Auth::user();
-      $authedUser->avatar = str_contains($authedUser->avatar, 'https') ? $authedUser->avatar : ($authedUser->avatar ? asset('storage/' . $authedUser->avatar) : null);
-      
-      $exercises = [];
-      $tournaments = [];
-      $evaluations = [];
-      
-      if (in_array($authedUser->role, ['Ne-Waza', 'Fighting'])) {
-//        $exercises = ExerciseAthlete::where('athlete_id', $authedUser->id)->get()->map(function ($exercise) {
-//          $exercise->exercise = Exercise::find($exercise->exercise_id);
-//          $exercise->date = Exercise::find($exercise->exercise_id)->date;
-//          return $exercise;
-//        });
-        $tournaments = Tournament::where('athlete_id', $authedUser->id)->get();
-        $evaluations = Evaluation::where('athlete_id', $authedUser->id)->get();
-      } else {
-        $exercises = Exercise::all();
-        $tournaments = Tournament::all();
-        $evaluations = Evaluation::all();
+      try {
+        $authedUser = Auth::user();
+        $authedUser->avatar = str_contains($authedUser->avatar, 'https')
+          ? $authedUser->avatar
+          : ($authedUser->avatar ? asset('storage/' . $authedUser->avatar) : null);
+
+        $exercises = [];
+        $tournaments = [];
+        $evaluations = [];
+
+        if (in_array($authedUser->role, ['Ne-Waza', 'Fighting'])) {
+          $tournaments = Tournament::where('athlete_id', $authedUser->id)->get();
+          $evaluations = Evaluation::where('athlete_id', $authedUser->id)->get();
+        } else {
+          $exercises = Exercise::all();
+          $tournaments = Tournament::all();
+          $evaluations = Evaluation::all();
+        }
+
+        $currentYear = date('Y');
+        $currentMonth = date('n');
+
+        return Inertia::render('Dashboard', [
+          'training_schedule' => TrainingSchedule::all(),
+          'unread_histories' => History::where('is_read', false)->get(),
+          'meta' => session('meta'),
+          'auth' => ['user' => $authedUser],
+          'stats' => [
+            'total_coaches' => Coach::count(),
+            'total_athletes' => Athlete::count(),
+            'total_exercises' => Exercise::count(),
+          ],
+          'athletes' => Athlete::with(['user', 'tournaments'])->get()->map(function ($athlete) {
+            $athlete->gold_medals = $athlete->tournaments->where('medal', 'Emas')->count();
+            $athlete->silver_medals = $athlete->tournaments->where('medal', 'Perak')->count();
+            $athlete->bronze_medals = $athlete->tournaments->where('medal', 'Perunggu')->count();
+            $athlete->total_medals = $athlete->gold_medals + $athlete->silver_medals + $athlete->bronze_medals;
+            return $athlete;
+          }),
+          'exercises' => $exercises,
+          'tournaments' => $tournaments,
+          'evaluations' => in_array($authedUser->role, ['Ne-Waza', 'Fighting'])
+            ? Evaluation::where('athlete_id', auth()->id())->get()
+            : Evaluation::distinct()->get(['athlete_id'])
+              ->pluck('athlete_id')
+              ->map(function ($athleteId) {
+                return User::where('id', $athleteId)->first(['id', 'avatar', 'full_name', 'role']);
+              })->filter()->values()->toArray(),
+          'reports' => Athlete::with(['exercises', 'tournaments', 'evaluations'])
+            ->where(function ($query) {
+              $query->has('exercises')
+                ->orWhereHas('evaluations')
+                ->orWhereHas('tournaments');
+            })->get(),
+          'ranking' => [
+            "Tahunan ($currentYear)" => Tournament::all()->groupBy('athlete_id')->map(function ($athleteTournaments, $athleteId) {
+              $athlete = $athleteTournaments->first()->athlete;
+              $gold_medals = $athleteTournaments->where('medal', 'Emas')->count();
+              $silver_medals = $athleteTournaments->where('medal', 'Perak')->count();
+              $bronze_medals = $athleteTournaments->where('medal', 'Perunggu')->count();
+              $total_medal_points = ($gold_medals * 3) + ($silver_medals * 2) + $bronze_medals;
+
+              return [
+                'full_name' => $athlete->user->full_name,
+                'gold_medals' => $gold_medals,
+                'silver_medals' => $silver_medals,
+                'bronze_medals' => $bronze_medals,
+                'total_medal_points' => $total_medal_points,
+              ];
+            })->values()->sort(function ($a, $b) {
+              if ($b['gold_medals'] === $a['gold_medals']) {
+                if ($b['silver_medals'] === $a['silver_medals']) {
+                  return $b['bronze_medals'] - $a['bronze_medals'];
+                }
+                return $b['silver_medals'] - $a['silver_medals'];
+              }
+              return $b['gold_medals'] - $a['gold_medals'];
+            })->take(3)->values()->toArray(),
+            "Bulanan ($currentMonth)" => Tournament::whereMonth('date', $currentMonth)->get()->groupBy('athlete_id')->map(function ($athleteTournaments, $athleteId) {
+              $athlete = $athleteTournaments->first()->athlete;
+              $gold_medals = $athleteTournaments->where('medal', 'Emas')->count();
+              $silver_medals = $athleteTournaments->where('medal', 'Perak')->count();
+              $bronze_medals = $athleteTournaments->where('medal', 'Perunggu')->count();
+              $total_medal_points = ($gold_medals * 3) + ($silver_medals * 2) + $bronze_medals;
+
+              return [
+                'full_name' => $athlete->user->full_name,
+                'gold_medals' => $gold_medals,
+                'silver_medals' => $silver_medals,
+                'bronze_medals' => $bronze_medals,
+                'total_medal_points' => $total_medal_points,
+              ];
+            })->values()->sort(function ($a, $b) {
+              if ($b['gold_medals'] === $a['gold_medals']) {
+                if ($b['silver_medals'] === $a['silver_medals']) {
+                  return $b['bronze_medals'] - $a['bronze_medals'];
+                }
+                return $b['silver_medals'] - $a['silver_medals'];
+              }
+              return $b['gold_medals'] - $a['gold_medals'];
+            })->take(3)->values()->toArray(),
+          ]
+        ]);
+      } catch (Exception $e) {
+        // Tampilkan detail error dan hentikan eksekusi
+        dd($e);
       }
-      
-      $currentYear = date('Y');
-      $currentMonth = date('n');
-      
-      return Inertia::render('Dashboard', [
-        'training_schedule' => TrainingSchedule::all(),
-        'unread_histories' => History::where('is_read', false)->get(),
-        'meta' => session('meta'),
-        'auth' => ['user' => $authedUser],
-        'stats' => [
-          'total_coaches' => Coach::count(),
-          'total_athletes' => Athlete::count(),
-          'total_exercises' => Exercise::count(),
-//          'total_tournaments' => Tournament::count(),
-//          'total_evaluations' => Evaluation::count(),
-        ],
-        'athletes' => Athlete::with(['user', 'tournaments'])->get()->map(function ($athlete) {
-          $athlete->gold_medals = $athlete->tournaments->where('medal', 'Emas')->count();
-          $athlete->silver_medals = $athlete->tournaments->where('medal', 'Perak')->count();
-          $athlete->bronze_medals = $athlete->tournaments->where('medal', 'Perunggu')->count();
-          $athlete->total_medals = $athlete->gold_medals + $athlete->silver_medals + $athlete->bronze_medals;
-          return $athlete;
-        }),
-        'exercises' => $exercises,
-        'tournaments' => $tournaments,
-        'evaluations' => in_array($authedUser->role, ['Ne-Waza', 'Fighting']) ? Evaluation::where('athlete_id', auth()->id())->get() : Evaluation::distinct()->get(['athlete_id'])
-          ->pluck('athlete_id')
-          ->map(function ($athleteId) {
-            return User::where('id', $athleteId)->first(['id', 'avatar', 'full_name', 'role']);
-          })->filter()->values()->toArray(),
-        'reports' => Athlete::with(['exercises', 'tournaments', 'evaluations'])
-          ->where(function ($query) {
-            $query->has('exercises')
-              ->orWhereHas('evaluations')
-              ->orWhereHas('tournaments');
-          })->get(),
-        'ranking' => [
-          "Tahunan ($currentYear)" => Tournament::all()->groupBy('athlete_id')->map(function ($athleteTournaments, $athleteId) {
-            $athlete = $athleteTournaments->first()->athlete;
-            $gold_medals = $athleteTournaments->where('medal', 'Emas')->count();
-            $silver_medals = $athleteTournaments->where('medal', 'Perak')->count();
-            $bronze_medals = $athleteTournaments->where('medal', 'Perunggu')->count();
-            $total_medal_points = ($gold_medals * 3) + ($silver_medals * 2) + $bronze_medals;
-            
-            return [
-              'full_name' => $athlete->user->full_name,
-              'gold_medals' => $gold_medals,
-              'silver_medals' => $silver_medals,
-              'bronze_medals' => $bronze_medals,
-              'total_medal_points' => $total_medal_points,
-            ];
-          })->values()->sort(function ($a, $b) {
-            if ($b['gold_medals'] === $a['gold_medals']) {
-              if ($b['silver_medals'] === $a['silver_medals']) {
-                return $b['bronze_medals'] - $a['bronze_medals'];
-              }
-              return $b['silver_medals'] - $a['silver_medals'];
-            }
-            return $b['gold_medals'] - $a['gold_medals'];
-          })->take(3)->values()->toArray(),
-          "Bulanan ($currentMonth)" => Tournament::whereMonth('date', $currentMonth)->get()->groupBy('athlete_id')->map(function ($athleteTournaments, $athleteId) {
-            $athlete = $athleteTournaments->first()->athlete;
-            $gold_medals = $athleteTournaments->where('medal', 'Emas')->count();
-            $silver_medals = $athleteTournaments->where('medal', 'Perak')->count();
-            $bronze_medals = $athleteTournaments->where('medal', 'Perunggu')->count();
-            $total_medal_points = ($gold_medals * 3) + ($silver_medals * 2) + $bronze_medals;
-            
-            return [
-              'full_name' => $athlete->user->full_name,
-              'gold_medals' => $gold_medals,
-              'silver_medals' => $silver_medals,
-              'bronze_medals' => $bronze_medals,
-              'total_medal_points' => $total_medal_points,
-            ];
-          })->values()->sort(function ($a, $b) {
-            if ($b['gold_medals'] === $a['gold_medals']) {
-              if ($b['silver_medals'] === $a['silver_medals']) {
-                return $b['bronze_medals'] - $a['bronze_medals'];
-              }
-              return $b['silver_medals'] - $a['silver_medals'];
-            }
-            return $b['gold_medals'] - $a['gold_medals'];
-          })->take(3)->values()->toArray(),
-        ]
-//        'rangking' => Tournament::with(['athlete.user'])->get()->groupBy(function ($tournament) {
-//          return Carbon::parse($tournament->date)->format('n-Y'); // 'n' removes leading zero from month
-//        })->map(function ($tournaments, $date) {
-//          $athletes = $tournaments->groupBy('athlete_id')->map(function ($athleteTournaments, $athleteId) {
-//            $athlete = $athleteTournaments->first()->athlete;
-//            $gold_medals = $athleteTournaments->where('medal', 'Emas')->count();
-//            $silver_medals = $athleteTournaments->where('medal', 'Perak')->count();
-//            $bronze_medals = $athleteTournaments->where('medal', 'Perunggu')->count();
-//            $total_medal_points = ($gold_medals * 3) + ($silver_medals * 2) + $bronze_medals;
-//
-//            return [
-//              'full_name' => $athlete->user->full_name,
-//              'gold_medals' => $gold_medals,
-//              'silver_medals' => $silver_medals,
-//              'bronze_medals' => $bronze_medals,
-//              'total_medal_points' => $total_medal_points,
-//            ];
-//          })->values()->toArray();
-//
-//          // Sort athletes by gold medals, then silver medals, then bronze medals
-//          usort($athletes, function ($a, $b) {
-//            if ($b['gold_medals'] === $a['gold_medals']) {
-//              if ($b['silver_medals'] === $a['silver_medals']) {
-//                return $b['bronze_medals'] - $a['bronze_medals'];
-//              }
-//              return $b['silver_medals'] - $a['silver_medals'];
-//            }
-//            return $b['gold_medals'] - $a['gold_medals'];
-//          });
-//
-//          // Limit to top 3 athletes
-//          $athletes = array_slice($athletes, 0, 3);
-//
-//          return [
-//            'date' => $date,
-//            'athletes' => $athletes,
-//          ];
-//        })->values()->toArray(),
-      ]);
     })->name('dashboard');
     
     Route::group(['prefix' => 'athletes'], function () {
