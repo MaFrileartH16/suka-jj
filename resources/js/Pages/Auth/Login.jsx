e887f4ef9c31a209f0c9524db5fceb20110be948
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-01-06 17:31:48 +0700

    update

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,88 +1,100 @@
-import { AppLayout } from '@/Layouts/AppLayout.jsx'
-import { Box, Button, Center, Checkbox, Flex, Image, SimpleGrid, TextInput, Title } from '@mantine/core'
-import { IconLock, IconLockOpen, IconMail, IconPassword } from '@tabler/icons-react'
-import { useForm } from '@inertiajs/react'
+import Checkbox from '@/Components/Checkbox';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import GuestLayout from '@/Layouts/GuestLayout';
+import { Head, Link, useForm } from '@inertiajs/react';
 
-const Login = (props) => {
-  const form = useForm({ email: '', password: '', remember: false })
-  
-  return (
-    <form onSubmit={(e) => {
-      e.preventDefault()
-      form.post(route('login'))
-    }}>
-      <AppLayout title="Log In" authed={props.auth.user} meta={props.meta}>
-        <SimpleGrid cols={{ base: 1, xs: 2 }} w="100%" h="100vh" p={16}>
-          
-          <Flex h={{ base: 120, xs: '100%' }} justify="center">
-            <Image fit="contain" src="https://pbjisurabaya.or.id/images/logo/pbji.png" />
-          </Flex>
-          
-          <Center>
-            <Box w={320}>
-              <Title mb={32}>Masuk Akun</Title>
-              
-              <TextInput variant="filled" leftSection={<IconMail />} styles={{
-                label: { marginBottom: 8 },
-                input: { height: 48, borderRadius: 32, paddingLeft: 50, paddingRight: 16 },
-                section: { marginLeft: 0, width: 48, height: 48 },
-                error: { marginTop: 8 },
-              }} label="Alamat Surel" placeholder="Masukkan alamat surel..." mb={16} onChange={(e) => {
-                const email = e.target.value.toLowerCase()
-                form.setData('email', email)
-                
-                if (!email) {
-                  form.setError({ email: 'Alamat surel tidak boleh kosong.' })
-                } else if (!/\S+@\S+\.\S+/.test(email)) {
-                  form.setError({ email: 'Alamat surel tidak sah.' })
-                } else {
-                  form.clearErrors('email')
-                }
-              }} error={form.errors.email} />
-              
-              <TextInput type="password" variant="filled" leftSection={<IconPassword />} styles={{
-                label: { marginBottom: 8 },
-                input: { height: 48, borderRadius: 32, paddingLeft: 50, paddingRight: 16 },
-                section: { marginLeft: 0, width: 48, height: 48 },
-                error: { marginTop: 8 },
-              }} label="Kata Sandi" placeholder="Masukkan kata sandi..." mb={16} onChange={(e) => {
-                form.setData('password', e.target.value)
-                
-                if (!e.target.value) {
-                  form.setError({ password: 'Kata sandi tidak boleh kosong.' })
-                } else {
-                  form.clearErrors('password')
-                }
-              }} error={form.errors.password} />
-              
-              <Checkbox
-                variant="filled"
-                mb={24}
-                styles={{
-                  label: { marginLeft: 16, fontSize: 14, padding: 0 },
-                  input: { border: 0, backgroundColor: form.data.remember ? 'var(--mantine-color-gold-2)' : '#f1f3f5' },
-                }}
-                radius={32}
-                label="Ingat Saya"
-                size="md"
-                color="gold.2"
-                onChange={(e) => form.setData('remember', e.target.checked)}
-              />
-              
-              <Button px={16} styles={{ section: { marginRight: 16 } }}
-                      leftSection={form.hasErrors || form.hasErrors || Object.entries(form.data).some(([key, value]) => key !== 'remember' && !value) ?
-                        <IconLock /> : <IconLockOpen />}
-                      disabled={form.hasErrors || Object.entries(form.data).some(([key, value]) => key !== 'remember' && !value)} loading={form.processing}
-                      variant="filled" type="submit"
-                      color="gold.2" h={48} fullWidth radius={32}>
-                Masuk
-              </Button>
-            </Box>
-          </Center>
-        </SimpleGrid>
-      </AppLayout>
-    </form>
-  )
+export default function Login({ status, canResetPassword }) {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        email: '',
+        password: '',
+        remember: false,
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route('login'), {
+            onFinish: () => reset('password'),
+        });
+    };
+
+    return (
+        <GuestLayout>
+            <Head title="Log in" />
+
+            {status && (
+                <div className="mb-4 text-sm font-medium text-green-600">
+                    {status}
+                </div>
+            )}
+
+            <form onSubmit={submit}>
+                <div>
+                    <InputLabel htmlFor="email" value="Email" />
+
+                    <TextInput
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={data.email}
+                        className="mt-1 block w-full"
+                        autoComplete="username"
+                        isFocused={true}
+                        onChange={(e) => setData('email', e.target.value)}
+                    />
+
+                    <InputError message={errors.email} className="mt-2" />
+                </div>
+
+                <div className="mt-4">
+                    <InputLabel htmlFor="password" value="Password" />
+
+                    <TextInput
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={data.password}
+                        className="mt-1 block w-full"
+                        autoComplete="current-password"
+                        onChange={(e) => setData('password', e.target.value)}
+                    />
+
+                    <InputError message={errors.password} className="mt-2" />
+                </div>
+
+                <div className="mt-4 block">
+                    <label className="flex items-center">
+                        <Checkbox
+                            name="remember"
+                            checked={data.remember}
+                            onChange={(e) =>
+                                setData('remember', e.target.checked)
+                            }
+                        />
+                        <span className="ms-2 text-sm text-gray-600">
+                            Remember me
+                        </span>
+                    </label>
+                </div>
+
+                <div className="mt-4 flex items-center justify-end">
+                    {canResetPassword && (
+                        <Link
+                            href={route('password.request')}
+                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                            Forgot your password?
+                        </Link>
+                    )}
+
+                    <PrimaryButton className="ms-4" disabled={processing}>
+                        Log in
+                    </PrimaryButton>
+                </div>
+            </form>
+        </GuestLayout>
+    );
 }
-
-export default Login
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,76 +1,28 @@
-import './bootstrap'
-import '../css/app.css'
-import '@mantine/core/styles.css'
-import '@mantine/notifications/styles.css'
-import '@mantine/dropzone/styles.css'
-import '@mantine/dates/styles.css'
-import '@mantine/tiptap/styles.css'
-import '@mantine/charts/styles.css'
+import '../css/app.css';
+import './bootstrap';
 
-import { createRoot } from 'react-dom/client'
-import { createInertiaApp } from '@inertiajs/react'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
-import { MantineProvider } from '@mantine/core'
-import { Notifications } from '@mantine/notifications'
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  title: (title) => `${title} ~ ${appName}`,
-  resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-  setup({ el, App, props }) {
-    const root = createRoot(el)
-    
-    root.render(
-      <MantineProvider
-        theme={{
-          black: '#0c0c0c',
-          // white: '#f3f3f3',
-          primaryColor: 'gold',
-          autoContrast: true,
-          fontFamily: 'Outfit, sans-serif',
-          colors: {
-            // primary: virtualColor({
-            //   name: 'primary',
-            //   light: 'red',
-            //   dark: 'netral'
-            // }),
-            neutral: [
-              '#0c0c0c',
-              '#232323',
-              '#3a3a3a',
-              '#515151',
-              '#686868',
-              '#808080',
-              '#979797',
-              '#aeaeae',
-              '#c5c5c5',
-              '#dcdcdc',
-              '#f3f3f3',
-            ],
-            gold: [
-              '#171400',
-              '#463b00',
-              '#746200',
-              '#a28900',
-              '#d1b000',
-              '#ffd700',
-              '#ffde2e',
-              '#ffe65d',
-              '#ffed8b',
-              '#fff4b9',
-              '#fffbe8',
-            ],
-          },
-        }}
-      >
-        <Notifications position="top-center" />
-        
-        <App {...props} />
-      </MantineProvider>,
-    )
-  },
-  progress: {
-    color: '#4B5563',
-  },
-})
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx'),
+        ),
+    setup({ el, App, props }) {
+        if (import.meta.env.SSR) {
+            hydrateRoot(el, <App {...props} />);
+            return;
+        }
+
+        createRoot(el).render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
